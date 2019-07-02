0d60b120c61e68355d85f314d26be771c26b1b95
susana araujo <26570969+sulamax@users.noreply.github.com>
2019-07-02 13:11:37 -0400

    Update typewritter.js

--- typewritter.js
+++ typewritter.js
@@ -1,4 +1,4 @@
-// const sentence = "hello there from lighthouse labs";
+const sentence = "hello there from lighthouse labs";
 
 // for (const char of sentence) {
 //   setTimeout(() => {
@@ -7,18 +7,10 @@
 //   }, 2000)
 // }
 
-setTimeout(function() {
-  process.stdout.write('h\n');
-}, 1000);
-setTimeout(function() {
-  process.stdout.write('e\n');
-}, 2000);
-setTimeout(function() {
-  process.stdout.write('l\n');
-}, 3000);
-setTimeout(function() {
-  process.stdout.write('l\n');
-}, 4000);
-setTimeout(function() {
-  process.stdout.write('o\n');
-}, 5000);
+
+
+for (const char in sentence) {
+  setTimeout(() => {
+    process.stdout.write(sentence[char]);
+  }, 100 + char * 100);
+};
